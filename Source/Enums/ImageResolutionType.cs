22b722abfacb47a66b267e2329992db11ea87077
Ricky Brundritt <ricky_brundritt@hotmail.com>
2017-05-05 11:46:58 -0700

    added new map styles

---
+++ Source/Enums/ImageResolutionType.cs
@@ -0,0 +1,18 @@
+﻿namespace BingMapsRESTToolkit
+{
+    /// <summary>
+    /// An enumeration for image resolution.
+    /// </summary>
+    public enum ImageResolutionType
+    {
+        /// <summary>
+        /// The default resolution.
+        /// </summary>
+        Default,
+
+        /// <summary>
+        /// High DPI resolution.
+        /// </summary>
+        High
+    }
+}
